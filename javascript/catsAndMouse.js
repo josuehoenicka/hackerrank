cc5e53318bfc12e3004b7f659c52ac06dcbdad1d
Josué Hoenicka <josuee.vzla@gmail.com>
2023-03-06 09:25:59 -0300

    create: add catsAndMouse.js

---
+++ javascript/catsAndMouse.js
@@ -0,0 +1,18 @@
+// You can see the challenge prompt at the following link below:
+// https://www.hackerrank.com/challenges/cats-and-a-mouse/problem?isFullScreen=true
+
+// My answer
+function catAndMouse(x, y, z) {
+    let catA = Math.abs(x - z);
+    let catB = Math.abs(y - z);
+    if(catA < catB){
+        return "Cat A";
+    } else if(catB < catA){
+        return "Cat B";
+    } else {
+        return "Mouse C";
+    }
+}
+
+// Test
+console.log(catAndMouse(1, 2, 3)); // "Cat B"
