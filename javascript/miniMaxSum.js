c0a3134fbe0d14c1a3fa9853081d2a6a67e2f34b
Josué Hoenicka <josuee.vzla@gmail.com>
2023-03-07 10:39:14 -0300

    update: finish miniMaxSum.js

--- javascript/miniMaxSum.js
+++ javascript/miniMaxSum.js
@@ -2,6 +2,20 @@
 // https://www.hackerrank.com/challenges/mini-max-sum/problem?isFullScreen=true
 
 // My answer
-
+function miniMaxSum(arr) {
+    let maxNum = Math.max(...arr);
+    let minNum = Math.min(...arr);
+    let resultMin = 0;
+    let resultMax = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] !== maxNum){
+            resultMin += arr[i];
+        } if(arr[i] !== minNum){
+            resultMax += arr[i];
+        }
+    }
+    console.log(resultMin, resultMax);
+}
 
 // Test
+miniMaxSum([1, 2, 3, 4, 5]) // 10 14
