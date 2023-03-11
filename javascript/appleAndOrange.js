745c68f3cf6caed72ab346cbfc6ab1ac3a812e6d
Josue Hoenicka <95772739+josuehoenicka@users.noreply.github.com>
2023-03-11 10:14:33 -0300

    update: finish appleAndOrange.js

--- javascript/appleAndOrange.js
+++ javascript/appleAndOrange.js
@@ -1,4 +1,13 @@
 // You can see the challenge prompt at the following link below:
 // https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true
 
-// To Do Saturday, 11 March
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    let appleCount = 0;
+    let orangeCount = 0;
+    const applePositions = apples.map((apple) => a + apple);
+    const orangePositions = oranges.map((orange) => b + orange);
+    appleCount = applePositions.filter((position) => position >= s && position <= t).length;
+    orangeCount = orangePositions.filter((position) => position >= s && position <= t).length;
+    console.log(appleCount + '\n' + orangeCount);
+  }
+  
